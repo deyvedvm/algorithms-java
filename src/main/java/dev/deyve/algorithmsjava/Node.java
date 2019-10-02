bf831d0958ed9bb45e0644e731aab06a825a8d3b
Deyve Vieira <deyvedvm@hotmail.com>
2019-10-01 21:27:51 -0300

    Algorithms - Tree

---
+++ src/main/java/dev/deyve/algorithmsjava/Node.java
@@ -0,0 +1,14 @@
+package dev.deyve.algorithmsjava;
+
+public class Node {
+
+    protected Integer value;
+
+    protected Node leftChild;
+
+    protected Node rightChild;
+
+    Node(Integer value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/dev/deyve/algorithmsjava/Tree.java
@@ -0,0 +1,35 @@
+package dev.deyve.algorithmsjava;
+
+public class Tree {
+
+    private Node root;
+
+    public void insert(int value) {
+        var node = new Node(value);
+
+        if (root == null) {
+            this.root = node;
+        }
+
+        var current = root;
+
+        while (true) {
+
+            if (value < current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = node;
+                    break;
+                }
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                }
+                current = current.rightChild;
+            }
+        }
+    }
+
+}
+
