a4bbb044c4a02597c1a0345fbe778119265b7def
Deyve Machado <deyve.machado@amedigital.com>
2020-01-27 15:21:17 -0300

    Streams: Filter Strings List

--- src/main/java/dev/deyve/algorithmsjava/streams/Streams.java
+++ src/main/java/dev/deyve/algorithmsjava/streams/Streams.java
@@ -1,6 +1,8 @@
 package dev.deyve.algorithmsjava.streams;
 
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * Java 8+ Streams
@@ -18,5 +20,21 @@ public class Streams {
         return users
                 .stream()
                 .reduce(0, (partialAgeResult, user) -> partialAgeResult + user.getAge(), Integer::sum);
+    }
+
+    /**
+     *
+     *  Filter String List
+     *
+     * @param stringList String List
+     * @param str String
+     * @return String
+     */
+    static List<String> filterStringList(List<String> stringList, String str){
+
+         return stringList
+                 .stream()
+                 .filter(string -> string.startsWith(str))
+                 .collect(Collectors.toList());
     }
 }
--- src/test/java/dev/deyve/algorithmsjava/streams/StreamsTest.java
+++ src/test/java/dev/deyve/algorithmsjava/streams/StreamsTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static dev.deyve.algorithmsjava.streams.Streams.filterStringList;
 import static dev.deyve.algorithmsjava.streams.Streams.sumValuesFromObject;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -27,7 +28,7 @@ class StreamsTest {
     }
 
     @Test
-    @DisplayName("sumValuesFromObject; List of Objects ; return a sum of values")
+    @DisplayName("Sum values from Object ; should receive a List of Objects ; return a sum of values")
     void sumValuesFromObjectTest() {
         List<User> users = Arrays.asList(john, julie);
 
@@ -36,5 +37,18 @@ class StreamsTest {
         Integer expectedResult = 77;
 
         assertEquals(result, expectedResult);
+    }
+
+    @Test
+    @DisplayName("Filter string list ; should receive a list of strings ; return a list of strings filtered")
+    void filterStringListTest(){
+
+        List<String> mockStrings = Arrays.asList("apples", "oranges", "pear", "bananas", "banana", "avocados", "apricots");
+
+        String mockString = "a";
+
+        List<String> result = filterStringList(mockStrings, mockString);
+
+        assertEquals(result, Arrays.asList("apples", "avocados", "apricots" ) );
     }
 }
