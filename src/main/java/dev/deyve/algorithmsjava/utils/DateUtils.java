5d1d7928badcf85234ed65fdff4bf16321f77324
Deyve Vieira <deyvedvm@hotmail.com>
2020-04-11 19:11:28 -0300

    Date Utils

---
+++ src/main/java/dev/deyve/algorithmsjava/utils/DateUtils.java
@@ -0,0 +1,45 @@
+package dev.deyve.algorithmsjava.utils;
+
+import java.time.*;
+
+/**
+ * Date Utils
+ */
+public class DateUtils {
+
+    /**
+     * Get ZonedDateTime from LocalDateTime and ZoneId
+     *
+     * @param localDateTime Date
+     * @param zoneId        ZoneId
+     * @return ZonedDateTime
+     */
+    static ZonedDateTime getLocalDateTimeWithZoneId(LocalDateTime localDateTime, ZoneId zoneId) {
+
+        return localDateTime.atZone(zoneId);
+    }
+
+    /**
+     * Get the same time instant in another zone
+     *
+     * @param zonedDateTime ZonedDateTime
+     * @param zoneId        ZoneId
+     * @return ZonedDateTime
+     */
+    static ZonedDateTime getSameInstantOtherZone(ZonedDateTime zonedDateTime, ZoneId zoneId) {
+
+        return zonedDateTime.withZoneSameInstant(zoneId);
+    }
+
+    /**
+     * Get Period of time between two LocalDates
+     *
+     * @param startDate LocalDate
+     * @param endDate   LocalDate
+     * @return Period
+     */
+    static Period getPeriodBetweenTwoDates(LocalDate startDate, LocalDate endDate) {
+
+        return Period.between(startDate, endDate);
+    }
+}
---
+++ src/test/java/dev/deyve/algorithmsjava/utils/DateUtilsTest.java
@@ -0,0 +1,53 @@
+package dev.deyve.algorithmsjava.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.*;
+
+import static dev.deyve.algorithmsjava.utils.DateUtils.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DateUtilsTest {
+
+    @Test
+    @DisplayName("Get ZonedDateTime from LocalDateTime and ZoneId")
+    void getLocalDateTimeWithZoneIdTest() {
+        ZoneId mockZoneId = ZoneId.of("America/Sao_Paulo");
+        LocalDateTime mockLocalDateTime = LocalDateTime
+                .of(2020, 4, 11, 18, 14, 0, 0);
+
+        ZonedDateTime result = getLocalDateTimeWithZoneId(mockLocalDateTime, mockZoneId);
+
+        ZonedDateTime expectedResult = ZonedDateTime
+                .of(2020, 4, 11, 18, 14, 0, 0, ZoneId.of("America/Sao_Paulo"));
+
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    void getSameInstantOtherZoneTest() {
+        ZoneId mockZoneId = ZoneId.of("America/Toronto");
+        ZonedDateTime mockZonedDateTime = ZonedDateTime
+                .of(2020, 4, 11, 18, 14, 0, 0, ZoneId.of("America/Sao_Paulo"));
+
+        ZonedDateTime result = getSameInstantOtherZone(mockZonedDateTime, mockZoneId);
+
+        ZonedDateTime expectedResult = ZonedDateTime
+                .of(2020, 4, 11, 17, 14, 0, 0, ZoneId.of("America/Toronto"));
+
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    void getPeriodBetweenTwoDatesTest() {
+        LocalDate startDate = LocalDate.of(2000, 2, 15);
+        LocalDate endDate = LocalDate.of(2020, 4, 11);
+
+        Period result = getPeriodBetweenTwoDates(startDate, endDate);
+
+        Period expectedResult = Period.of(20, 1, 27);
+
+        assertEquals(result, expectedResult);
+    }
+}
