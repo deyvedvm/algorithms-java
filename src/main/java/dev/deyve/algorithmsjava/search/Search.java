0589984814e300edbabfe59861aa29d082964e53
Deyve Vieira <deyvedvm@hotmail.com>
2020-04-23 11:32:05 -0300

    BubbleSort using generics

--- src/main/java/dev/deyve/algorithmsjava/search/Search.java
+++ src/main/java/dev/deyve/algorithmsjava/search/Search.java
@@ -25,4 +25,31 @@ public class Search {
         return -1;
     }
 
+    public static <T extends Comparable<T>> Integer bubbleSort(List<T> array) {
+
+        for (int lastUnsortedIndex = array.size() - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+
+                if (array.indexOf(i) > array.indexOf(i + 1)) {
+
+                    swap(array, i, i + 1);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static <T extends Comparable<T>> void swap(List<T> array, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        T temp = array.get(i);
+        array.set(i, array.get(j));
+        array.set(j, temp);
+    }
+
 }
