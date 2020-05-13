f6caedceeb84c30e2954fe6dbc98c57e2c038c83
Deyve Vieira <deyvedvm@hotmail.com>
2020-05-12 23:56:10 -0300

    Post - Equals and HashCode

--- src/main/java/dev/deyve/algorithmsjava/objects/Main.java
+++ src/main/java/dev/deyve/algorithmsjava/objects/Main.java
@@ -1,17 +1,27 @@
 package dev.deyve.algorithmsjava.objects;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Person person = new Person(1, "Deyve");
-        Person samePerson = new Person(1, "Deyve");
+        Person person = new Person(1, "Senna");
+        Person samePerson = new Person(1, "Senna");
 
-        System.out.println(person);
-        System.out.println("HashCode: " + person.hashCode());
-        System.out.println(samePerson);
-        System.out.println("HashCode: " + samePerson.hashCode());
+//        System.out.println("Person: " + person + " HashCode: " + person.hashCode());
+//        System.out.println("Person: " + samePerson + " HashCode: " + samePerson.hashCode());
 
-        System.out.println(person.equals(samePerson));
+//        System.out.println("Are Equals?: " + person.equals(samePerson));
+
+        Set<Person> persons = new HashSet<>();
+        persons.add(person);
+        persons.add(samePerson);
+
+        System.out.println("Person: " + person + " HashCode: " + person.hashCode());
+        System.out.println("Person: " + samePerson + " HashCode: " + samePerson.hashCode());
+        System.out.println("Set size: " + persons.size());
+        System.out.println("Set contains: Senna: " + persons.contains(new Person(1, "Senna")));
     }
 }
--- src/main/java/dev/deyve/algorithmsjava/objects/Person.java
+++ src/main/java/dev/deyve/algorithmsjava/objects/Person.java
@@ -1,14 +1,50 @@
 package dev.deyve.algorithmsjava.objects;
 
-import lombok.AllArgsConstructor;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @ToString
-@AllArgsConstructor
 public class Person {
 
     private final Integer id;
 
     private final String name;
 
+    public Person(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final Person other = (Person) obj;
+        if (!this.id.equals(other.id)) {
+            return false;
+        }
+
+        return Objects.equals(this.name, other.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        int hash = 7;
+        hash = 79 * hash + this.id;
+        hash = 79 * hash + Objects.hashCode(this.name);
+
+        return hash;
+    }
 }
