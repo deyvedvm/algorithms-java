bcbbc3686b68cee1e5d2794d4f322b880607b161
Deyve Vieira <deyvedvm@hotmail.com>
2021-01-17 22:02:00 -0300

    Tree

--- src/main/java/dev/deyve/algorithmsjava/trees/Tree.java
+++ src/main/java/dev/deyve/algorithmsjava/trees/Tree.java
@@ -40,5 +40,111 @@ public class Tree {
         }
     }
 
+    public boolean find(int value) {
+        var current = root;
+
+        while (current != null) {
+            if (value < current.value) {
+                current = current.leftChild;
+            } else if (value > current.value) {
+                current = current.rightChild;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        traverseInOrder(root.leftChild);
+        System.out.println(root.value);
+        traverseInOrder(root.rightChild);
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void traversePostOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.println(root.value);
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node root) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (isLeaf(root)) {
+            return 0;
+        }
+
+        return 1 + Math.max(
+                height(root.leftChild),
+                height(root.rightChild));
+    }
+
+    // O(log n)
+    public int minBinarySearch() throws IllegalAccessException {
+        if (root == null) throw new IllegalAccessException();
+
+        var current = root;
+        var last = current;
+
+        while (current != null) {
+            last = current;
+            current = current.leftChild;
+        }
+        return last.value;
+    }
+
+    // O(n)
+    public int min() {
+        return min(root);
+    }
+
+    private int min(Node root) {
+        if (isLeaf(root)) {
+            return root.value;
+        }
+
+        var left = min(root.leftChild);
+        var right = min(root.rightChild);
+
+        return Math.min(Math.min(left, right), root.value);
+    }
+
+    private boolean isLeaf(Node node) {
+        return node.leftChild == null && node.rightChild == null;
+    }
 }
 
--- src/test/java/dev/deyve/algorithmsjava/trees/TreeTest.java
+++ src/test/java/dev/deyve/algorithmsjava/trees/TreeTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tree Test
@@ -16,35 +16,49 @@ class TreeTest {
     @BeforeEach
     void setUp() {
         tree = new Tree();
-    }
-
-    @Test
-    @DisplayName("Should insert 5 in the root node")
-    void shouldInsertOneNodeWith() {
 
         tree.insert(5);
-
-        assertEquals(tree.getRoot().value.toString(), "5");
     }
 
     @Test
     @DisplayName("Should insert 3 in the left node")
-    void shouldInsertTwoNodes() {
+    void shouldInsertTwoNodesTest() {
 
-        tree.insert(5);
         tree.insert(3);
 
-        assertEquals(tree.getRoot().leftChild.value.toString(), "3");
+        assertEquals(tree.getRoot().leftChild.value, 3);
     }
 
     @Test
     @DisplayName("Should insert 8 in the right node")
-    void shouldInsertThreeNodes() {
+    void shouldInsertThreeNodesTest() {
 
-        tree.insert(5);
+        tree.insert(8);
+
+        assertEquals(tree.getRoot().rightChild.value, 8);
+    }
+
+    @Test
+    @DisplayName("Should find 3 in the tree")
+    void shouldFindRootNodeTest() {
+
         tree.insert(3);
         tree.insert(8);
 
-        assertEquals(tree.getRoot().rightChild.value.toString(), "8");
+        boolean result = tree.find(3);
+
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Should not find 10 in the tree")
+    void shouldNotFindRootNodeTest() {
+
+        tree.insert(3);
+        tree.insert(8);
+
+        boolean result = tree.find(10);
+
+        assertFalse(result);
     }
 }
